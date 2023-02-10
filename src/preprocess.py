b5d55b809f7774976823336b97028db3e92bb684
jonas-mika <accounts@jonassenghaas.de>
2023-02-10 11:42:28 +0100

    feat(preprocessing): using moviepy for loading and clipping raw videos

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,38 +2,68 @@
 #  by: mika senghaas
 
 import os
-from tqdm import trange
-from pytorchvideo.data.encoded_video import EncodedVideo
 
-from utils import load_video, load_annotations
+from tqdm import trange
+
 from config import *
+from utils import (
+    clip_video,
+    end_task,
+    get_label,
+    load_annotations,
+    load_metadata,
+    load_video,
+    start_task,
+)
 
 def main():
+    """
+    Load annotations and video from RAW_DATA_PATH directory where each video-annotation pairing
+    is saved in a directory with the date of recording called `DDMMYY`. The video is called 
+    `video.mov` and the annotations is a text file called `annotations`.
+
+    The video and annotations are loaded and depending split into clips of length (s)
+    CLIP_LENGTH. For each clip, the label (location denoting floor and corner at ITU) is extracted,
+    so that the clip can be saved into PROCESSED_DATA_PATH into a directory with the name of the label
+    and the clip called `DDMMYY_XY.mov` where `XY` is the clip number.
+
+    Efficiency: ~7s for extrating 1 labelled 10s clip
+    """
+    start_timer = start_task("Processing Raw Videos", get_timer=True)
+
     # read all directories in raw data folder
     directories = os.listdir(RAW_DATA_PATH)
 
     # iterate over all videos
-    for directory in directories:
-        # open video and annotations
-        video : EncodedVideo = load_video(directory)
-        annotations : Annotation = load_annotations(directory)
+    for date in directories:
+        directory = f"{RAW_DATA_PATH}/{date}"
+        video_path = f"{directory}/video.mov"
+        annotation_path = f"{directory}/annotations"
 
-        # iterate over video frame by frame
-        pbar = trange(10)
-        for frame in pbar:
-            # get 10x10s clips
-            clip = video.get_clip(10*frame, 10*frame+10)
+        # load metadata
+        meta : dict = load_metadata(video_path)
+        duration : int = eval(meta['duration'])
 
-            if clip:
-                video_tensor = clip.get('video')
-                print(video_tensor.shape)
-                # compute label for frame from annotations
-                # label = get_label(frame_num, annotations)
-                # frame = transform_frame(frame)
+        # load video and meta information
+        video = load_video(video_path)
+        annotations : Annotation = load_annotations(annotation_path)
 
-                #write_frame(frame, label, frame_num)
+        # iterate over clips
+        pbar = trange(0, int(duration)-CLIP_LENGTH, CLIP_LENGTH)
+        total_clips = (int(duration)-CLIP_LENGTH) // CLIP_LENGTH
+        for clip_num, start_time in enumerate(pbar):
+            pbar.set_description(f"Extracting {directory}.MOV ({clip_num+1}/{total_clips})")
+            end_time = start_time+CLIP_LENGTH
 
-        pbar.set_description( f"Extracting {directory}.MOV ({frame_num}/{frame_count})")
+            # extract label depending on clip time
+            label = get_label(start_time + CLIP_LENGTH//2, annotations)
+            destination_path = f"{PROCESSED_DATA_PATH}/{label}/{date}_{str(clip_num).zfill(2)}.mov"
+
+            # save clip from video according to label and start/ end time
+            clip_video(video, dst=destination_path, start_time=start_time, end_time=end_time)
+
+        video.close()
+        end_task("Processing Raw Videos", start_timer)
 
 if __name__ == "__main__":
     main()
--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,41 @@
 # utils.py
 #  by: mika senghaas
 
-# import torchvision
-from matplotlib import pyplot as plt
-from pytorchvideo.data.encoded_video import EncodedVideo
+from multiprocessing import cpu_count
+import os
+import timeit
+import datetime
+
+import ffmpeg
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from termcolor import colored
 
 from config import *
 
-def load_video(date : str) -> EncodedVideo:
-    filepath = f"{RAW_DATA_PATH}/{date}/{date}.MOV"
-    return EncodedVideo.from_path(filepath, decode_audio=False)
+def mkdir(filepath: str) -> bool:
+    if filepath.find('.') >= 0:
+        filepath = '/'.join(filepath.split('/')[:-1])
+    if not os.path.exists(filepath):
+        os.makedirs(filepath)
+        return True
+    return False
 
-def load_annotations(date : str) -> Annotation:
+def start_task(task: str, get_timer: bool = False) -> float | None:
+    print(colored(f"> {task}", "green"))
+    return timeit.default_timer() if get_timer else None
+
+def end_task(task: str, start_time : float | None = None) -> None:
+    if start_time:
+        print(colored(f"> Finished {task} in {datetime.timedelta(seconds=int(timeit.default_timer() - start_time))}", "green"))
+    else:
+        print(colored(f"> Finished {task}", "green"))
+
+def load_metadata(filepath: str) -> dict:
+    meta = ffmpeg.probe(filepath).get('format')
+    return meta
+
+def load_annotations(filepath : str) -> Annotation:
     targets  = []
-    filepath = f"{RAW_DATA_PATH}/{date}/annotations"
     with open(filepath, "r") as file:
         for line in file:
             timestamp, label = line.strip().split(',')
@@ -21,42 +43,31 @@ def load_annotations(date : str) -> Annotation:
 
     return targets 
 
-def timestamp_to_frame(timestamp : str) -> int:
-  mm, ss = map(int, timestamp.split(':'))
-  return 30 * (mm * 60 + ss)
+def load_video(src: str):
+    return VideoFileClip(src, audio=False, target_resolution=(1920, 1080))
 
-def get_label(frame_num: int, annotations: list[tuple[str, str]]) -> str:
+def clip_video(video: VideoFileClip, dst: str, start_time: int, end_time: int) -> None :
+    assert end_time > start_time, "End Time must be larger thane Start Time"
+    if mkdir(dst):
+        clip = video.subclip(start_time, end_time)
+        clip.write_videofile(dst, 
+                audio=False, 
+                logger=None,
+                codec="libx264", 
+                threads=cpu_count())
+
+def timestamp_to_second(timestamp : str) -> int:
+  mm, ss = map(int, timestamp.split(':'))
+  return mm * 60 + ss
+
+def get_label(second_in_video: int, annotations: list[tuple[str, str]]) -> str:
   # assumes targets is sorted by timestamp
   prev_label = annotations[0][1]
   for i in range(1, len(annotations)):
     timestamp, label = annotations[i]
-    framecount = timestamp_to_frame(timestamp)
-    if frame_num < framecount:
+    seconds = timestamp_to_second(timestamp)
+    if second_in_video < seconds:
       return prev_label
     prev_label = label
 
   return annotations[-1][1]
-
-def visualize_batch(batch, batch_size, classes, dataset_type):
-  # initialize a figure
-  fig = plt.figure("{} batch".format(dataset_type), figsize=(batch_size, batch_size))
-  # loop over the batch size
-  for i in range(0, batch_size):
-    # create a subplot
-    ax = plt.subplot(2, 4, i + 1)
-    # grab the image, convert it from channels first ordering to
-    # channels last ordering, and scale the raw pixel intensities
-    # to the range [0, 255]
-    image = batch[0][i].cpu().numpy()
-    image = image.transpose((1, 2, 0))
-    image = (image * 255.0).astype("uint8")
-    # grab the label id and get the label from the classes list
-    idx = batch[1][i]
-    label = classes[idx]
-    # show the image along with the label
-    plt.imshow(image)
-    plt.title(label)
-    plt.axis("off")
-  # show the plot
-  plt.tight_layout()
-  plt.show()
