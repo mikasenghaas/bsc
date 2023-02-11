1e1633073a658bb3ad5d10998bacbc6691befc75
jonas-mika <accounts@jonassenghaas.de>
2023-02-11 17:42:57 +0100

    feat(config): minor changes

--- src/config.py
+++ src/config.py
@@ -1,30 +1,34 @@
 # config.py
 #  by: mika senghaas
 
+import os
 import torch
 from typing import NewType
 
 # PATHS
-RAW_DATA_PATH : str = "src/data/raw"
-PROCESSED_DATA_PATH : str = "src/data/processed"
+RAW_DATA_PATH : str = os.path.join("src", "data", "raw")
+PROCESSED_DATA_PATH : str = os.path.join("src", "data", "processed")
+MODEL_PATH : str = os.path.join("src", "models")
 
 # PREPROCESS
-CLIP_LENGTH : int = 10 # video length
-FPS : int = 4 # downsample to 25% of frames
-H :int = 480 # image height
-W : int = 270 # image width
-C : int = 3 # num channels (RGB)
+FPS : int = 2 # frames per second to sample
+
+HEIGHT :int = 224 # image height
+WIDTH : int = 224 # image width
+CHANNELS : int = 3 # num channels (RGB)
 
 # MODEL
+CHECKPOINT = "MCG-NJU/videomae-base"
+NUM_FRAMES = 16 # number of frames to sample
 
 # OPTIMISER
 LR : float = 1e-3
 
 # TRAINING
 DEVICE : str = 'cuda' if torch.cuda.is_available() else 'cpu'
-MAX_ITERS : int = 10 ** 5
+MAX_EPOCHS : int = 5
 EVAL_ITERS : int = 100
-BATCH_SIZE : int = 16
+BATCH_SIZE : int = 2
 
 # TYPES
 Annotation = NewType("AnnotationType", list[tuple[str, str]])
