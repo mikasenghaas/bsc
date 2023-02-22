7febbde0d46614de1335543ef41d5c75a2326b46
jonas-mika <accounts@jonassenghaas.de>
2023-02-22 13:48:30 +0100

    feat(config): added mps device for training on m1

--- src/config.py
+++ src/config.py
@@ -25,7 +25,8 @@ NUM_FRAMES = 16 # number of frames to sample
 LR : float = 1e-3
 
 # TRAINING
-DEVICE : str = 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE : str = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+
 MAX_EPOCHS : int = 5
 EVAL_ITERS : int = 100
 BATCH_SIZE : int = 2
