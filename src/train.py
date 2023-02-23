dd467fe0d800bd4ee94417b827cb968e314c72f7
jonas-mika <accounts@jonassenghaas.de>
2023-02-23 16:15:06 +0100

    feat(train): extracted training into train function and implemented argparse

--- src/train.py
+++ src/train.py
@@ -5,61 +5,82 @@ import datetime
 
 import torch
 import torch.nn as nn
-from torch.nn.functional import softmax
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from config import *
-from data import TrainData
-from model import VideoClassifier
+from data import ImageDataset
+from model import ResNet
 from utils import *
 
-def main():
-    start_timer = start_task("Running train.py", get_timer=True)
-
-    # load model and dataset
-    start_task("Initialising Model")
-    model = VideoClassifier()
-
-    start_task("Initialising Training Data")
-    data = TrainData(filepath=PROCESSED_DATA_PATH, model=model)
-    # prior: one clip: 2.408.448 float32 = 77.070.336 Bits = 9 MB
-
-    loader = DataLoader(data, batch_size=BATCH_SIZE)
-
-    # load conversions
-    label2id, id2label = model.label2id, model.id2label
-
-    # optimiser and
-    optim = torch.optim.Adam(model.parameters(), lr=LR)
-    criterion = nn.CrossEntropyLoss()
-
-    # start training
-    start_task("Starting Training Loop")
-    for epoch in range(MAX_EPOCHS):
-        pbar = tqdm(loader)
-        batch = 0
-        pbar.set_description(f"Epoch {epoch+1}/{MAX_EPOCHS} - Batch {batch}/{len(loader)} - 0.000")
-        for batch, (x, y) in enumerate(pbar):
-            # x: (B, T, C, H, W); y: B
-
-            # predict class
-            logits = model(x)
-            probs = softmax(logits)
-
-            # compute loss
-            loss = criterion(probs, y)
+def train(model, train_loader, criterion, optim, scheduler, max_epochs, val_loader=None):
+    model.to(DEVICE)
+    model.train()
+    pbar = tqdm(range(max_epochs))
+    pbar.set_description(f'EPOCH X - BATCH X - LOSS X.XXX - ACC XX.X%')
+    for epoch in pbar:
+        running_loss = 0.0
+        running_correct = 0
+    
+        for batch_num, (inputs, labels) in enumerate(train_loader):
+            inputs = inputs.to(DEVICE)
+            labels = labels.to(DEVICE)
+  
+            # zero the parameter gradients
+            optim.zero_grad()
+  
+            logits = model(inputs)
+            _, preds = torch.max(logits, 1)
+            loss = criterion(logits, labels)
+  
+            # backward + optimize only if in training phase
             loss.backward()
             optim.step()
+            scheduler.step()
 
-            pbar.set_description(f"Epoch {epoch+1}/{MAX_EPOCHS} - Batch {batch+1}/{len(loader)} - {round(loss.item(), 3)}")
-            batch += 1
+            running_loss += loss.item() * inputs.size(0)
+            running_correct += torch.sum(labels == preds)
+  
+            samples_seen = (batch_num + 1) * BATCH_SIZE
+            pbar.set_description(f'EPOCH {epoch} - BATCH {batch_num} - LOSS {(running_loss / samples_seen):.3f} - ACC {(running_correct / samples_seen * 100):.1f}%')
 
+    return model
 
-    start_task("Saving Model")
-    mkdir(MODEL_PATH)
-    save_path = f"{MODEL_PATH}/{datetime.datetime.now()}"
-    torch.save(model.state_dict(), save_path)
+def main():
+    parser = load_train_parser()
+    args = parser.parse_args()
+
+    start_timer = start_task("Running train.py", get_timer=True)
+
+    # load data 
+    start_task("Initialising Data and Model")
+    match args.model:
+        case 'resnet':
+            train_data = ImageDataset(filepath=PROCESSED_DATA_PATH, split="train")
+            model = ResNet(train_data.num_classes)
+        case _:
+            # default case
+            train_data = ImageDataset(filepath=PROCESSED_DATA_PATH, split="train")
+            model = ResNet(train_data.num_classes)
+
+    # initialise data loader
+    train_loader = DataLoader(train_data, batch_size=args.batch_size)
+
+    # define loss func, optimiser and lr scheduler
+    criterion = nn.CrossEntropyLoss() # pyright: ignore
+    optim = torch.optim.Adam(model.parameters(), lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optim, args.step_size, args.gamma)
+
+    # train model
+    start_task("Starting Training")
+    trained_model = train(model, train_loader, criterion, optim, scheduler, args.max_epochs)
+
+    # save model
+    if args.save:
+        start_task("Saving Model")
+        mkdir(MODEL_PATH)
+        save_path = os.path.join(MODEL_PATH, str(datetime.datetime.now()))
+        torch.save(trained_model.state_dict(), save_path)
 
     end_task("Training Done", start_timer)
 
