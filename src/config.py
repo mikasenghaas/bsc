8e04d3d6e0b03e8bcd3588a87dc13c5ad4665665
jonas-mika <accounts@jonassenghaas.de>
2023-02-23 15:01:02 +0100

    feat(config): updated config
    deleted typing, added training hyperparameters and formatted for better readability

--- src/config.py
+++ src/config.py
@@ -3,33 +3,30 @@
 
 import os
 import torch
-from typing import NewType
 
 # PATHS
-RAW_DATA_PATH : str = os.path.join("src", "data", "raw")
-PROCESSED_DATA_PATH : str = os.path.join("src", "data", "processed")
-MODEL_PATH : str = os.path.join("src", "models")
+RAW_DATA_PATH : str         = os.path.join("src", "data", "raw")
+PROCESSED_DATA_PATH : str   = os.path.join("src", "data", "processed")
+MODEL_PATH  : str           = os.path.join("src", "models")
 
 # PREPROCESS
-FPS : int = 2 # frames per second to sample
+HEIGHT      : int           = 224 # image height
+WIDTH       : int           = 224 # image width
+MEAN        : torch.Tensor  = torch.tensor([0.485, 0.456, 0.406])
+STD         : torch.Tensor  = torch.tensor([0.229, 0.224, 0.225])
 
-HEIGHT :int = 224 # image height
-WIDTH : int = 224 # image width
-CHANNELS : int = 3 # num channels (RGB)
-
-# MODEL
-CHECKPOINT = "MCG-NJU/videomae-base"
-NUM_FRAMES = 16 # number of frames to sample
-
-# OPTIMISER
-LR : float = 1e-3
+# DATA
+SPLITS      : list[str]     = ["train", "val", "test"]
+TRAIN_RATIO : float         = 0.7
+VAL_RATIO   : float         = 0.1
+TEST_RATIO  : float         = 0.2
 
 # TRAINING
-DEVICE : str = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-
-MAX_EPOCHS : int = 5
-EVAL_ITERS : int = 100
-BATCH_SIZE : int = 2
-
-# TYPES
-Annotation = NewType("AnnotationType", list[tuple[str, str]])
+DEVICE      : str           = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+MAX_EPOCHS  : int           = 30
+BATCH_SIZE  : int           = 16
+LR          : float         = 1e-3
+STEP_SIZE   : int           = 7
+GAMMA       : float         = 0.1
+SAVE        : bool          = False
+FINETUNE    : bool          = True
