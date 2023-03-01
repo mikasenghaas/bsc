e3db26fa8e9554d206040188fe53871bd74a9564
jonas-mika <accounts@jonassenghaas.de>
2023-03-01 21:19:19 +0100

    feat(config): restructured config, added pretraining arg

--- src/config.py
+++ src/config.py
@@ -5,33 +5,38 @@ import os
 import torch
 
 # PATHS
-RAW_DATA_PATH : str         = os.path.join("src", "data", "raw")
-PROCESSED_DATA_PATH : str   = os.path.join("src", "data", "processed")
-MODEL_PATH  : str           = os.path.join("src", "models")
+RAW_DATA_PATH : str                 = os.path.join("src", "data", "raw")
+PROCESSED_DATA_PATH : str           = os.path.join("src", "data", "processed")
+MODEL_PATH  : str                   = os.path.join("src", "models")
+
+# GENERAL
+DEVICE      : str                   = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
 # PREPROCESS
-# NUM_FRAMES  : int           = 16
-MAX_LENGTH  : int           = 10 
-FPS         : int           = 2
-HEIGHT      : int           = 224
-WIDTH       : int           = 224
-MEAN        : torch.Tensor  = torch.tensor([0.485, 0.456, 0.406])
-STD         : torch.Tensor  = torch.tensor([0.229, 0.224, 0.225])
+# NUM_FRAMES  : int                  = 16
+MAX_LENGTH  : int                   = 10 
+FPS         : int                   = 2
+HEIGHT      : int                   = 224
+WIDTH       : int                   = 224
 
 # DATA
-SPLITS      : list[str]     = ["train", "val", "test"]
-TRAIN_RATIO : float         = 0.7
-VAL_RATIO   : float         = 0.1
-TEST_RATIO  : float         = 0.2
+SPLITS      : list[str]             = ["train", "val", "test"]
+TRAIN_RATIO : float                 = 0.7
+VAL_RATIO   : float                 = 0.1
+TEST_RATIO  : float                 = 0.2
+
+# TRANSFORMS
+MEAN        : torch.Tensor          = torch.tensor([0.485, 0.456, 0.406])
+STD         : torch.Tensor          = torch.tensor([0.229, 0.224, 0.225])
 
 # TRAINING
-DEVICE      : str           = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-MAX_EPOCHS  : int           = 30
-BATCH_SIZE  : int           = 16
-LR          : float         = 1e-3
-STEP_SIZE   : int           = 5
-GAMMA       : float         = 0.1
-LOG         : bool          = False
+PRETRAINED  : bool                  = True
+MAX_EPOCHS  : int                   = 30
+BATCH_SIZE  : int                   = 32
+LR          : float                 = 1e-3
+STEP_SIZE   : int                   = 5
+GAMMA       : float                 = 0.1
+LOG         : bool                  = False
 
 # EVALUATE
-DURATION    : int           = 10
+DURATION    : int                   = 10
