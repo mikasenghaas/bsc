f74743850f342c7fef3e572fbd1a9e1121e257a4
jonas-mika <accounts@jonassenghaas.de>
2023-03-06 11:46:55 +0100

    feat(data): updated ImageDataset
    now supports only sampling subset of classes, fewer samples per class; general code refactoring for
    instatantiation

--- src/data.py
+++ src/data.py
@@ -4,45 +4,100 @@
 import glob
 import random
 import torchvision
-from torchvision import transforms
 from torch.utils.data import Dataset
 
 from config import *
 from utils import *
 
+# random.seed(SEED)
+
 # load data
 class ImageDataset(Dataset):
-    def __init__(self, filepath: str, split = "train"):
-        self.filepath = filepath
-        self.split = split
+    """
+    ImageDataset extends the default torch.utils.Dataset class to sample random frames
+    from an image dataset. Upon instantiation it loads all image paths to memory given
+    a filepath to a directory, with the following expected structure:
 
-        # image paths
-        self.image_paths = load_labelled_image_paths(filepath)
-        self.total_samples = len(self.image_paths)
+    __filepath:
+      |__ class1
+          |__ frame1.jpg
+          |__ ...
+      |__ ...
+
+    Parameters:
+
+    split : str { train, val, test } = Samples from train, validation or test split
+    include_classes : list[str] = List of classes to include 
+    ratio : float = Randomly sample ratio samples within each class
+    """
+    @staticmethod
+    def default_config():
+        """
+        Staticmethod that contains all expected arguments for initialising a ImageDataset 
+        object. The values are taken from config.py. Use this method either on the the class like
+        ImageDataset.default_config()
+        """
+        # default configuration for ImageDataset
+        split : str = SPLIT
+        include_classes : list[str] = sorted(CLASSES)
+        ratio : float = RATIO
+
+        return {"split": split,
+                "include_classes": include_classes,
+                "ratio": ratio, }
+
+    def __init__(self, **kwargs):
+        assert len(kwargs.keys()) > 0 and all(x in kwargs.keys() for x in ImageDataset.default_config().keys()), f"Class needs to be initialised with default values for 'filepath', 'split', 'include_classes' and 'ratio'."
+
+        # save kwargs into variables
+        self.split = kwargs['split']
+        self.include_classes = kwargs['include_classes']
+        self.ratio = kwargs['ratio']
+
+        # pre conditions
+        assert self.split in SPLITS, "Split must be either 'train', 'val' or 'test'"
+        assert all(x in CLASSES for x in self.include_classes), f"Only include classes from {CLASSES}"
+        assert self.ratio > 0.0 and self.ratio <= 1.0, "Ratio needs to be in ]0,1]"
+
+        # get all image paths by class
+        self.images_by_class : dict[str, list[str]] = load_labelled_image_paths(PROCESSED_DATA_PATH)
+
+        # subset to only include specified classes
+        self.images_by_class = {k: self.images_by_class[k] for k in self.include_classes if k in self.images_by_class}
+        assert len(self.images_by_class) == len(self.include_classes)
+
+        # randomly sample num_classes
+        if self.ratio != 1.0:
+            for key, val in self.images_by_class.items():
+                n = int(len(val) * self.ratio)
+                self.images_by_class[key] = random.sample(val, n)
+
+        # convert to shuffled flattened list of paths
+        image_paths = [image_paths for image_paths in self.images_by_class.values()]
+        self.image_paths = [item for sublist in image_paths for item in sublist] # flatten
         random.shuffle(self.image_paths)
 
-        # transforms
-        self.normalise = transforms.Normalize(MEAN, STD) # pyright:ignore
-
-        # labels
-        self.labels = load_labels(PROCESSED_DATA_PATH)
-        self.num_classes = len(self.labels)
-        self.label2id = { l: i for i, l in enumerate(self.labels) }
-        self.id2label = { i: l for i, l in enumerate(self.labels) }
+        # meta
+        self.class_distribution = { k: len(v) for k, v in self.images_by_class.items()}
+        self.classes = list(self.class_distribution.keys())
+        self.num_classes = len(self.classes)
+        self.num_samples = len(self.image_paths)
+        self.class2id = { l: i for i, l in enumerate(self.classes) }
+        self.id2class = { i: l for i, l in enumerate(self.classes) }
 
         # meta information to log
-        self.meta = {
-                'Labels': self.labels,
-                'Number of Samples': self.total_samples,
-                'Number of Classes': self.num_classes,
-                'Splits': [TRAIN_RATIO, VAL_RATIO, TEST_RATIO]
-                }
+        self.meta = kwargs
+        self.meta.update({
+                'class_distribution': self.class_distribution,
+                'number_of_samples': self.num_samples,
+                'number_of_classes': self.num_classes,
+                })
 
     def __getitem__(self, idx): # [x1, ..., x10]
         if self.split == 'val':
-            idx += int(self.total_samples * TRAIN_RATIO)
+            idx += int(self.num_samples * TRAIN_RATIO)
         elif self.split == 'test':
-            idx += int(self.total_samples * TRAIN_RATIO) + int(self.total_samples * VAL_RATIO)
+            idx += int(self.num_samples * TRAIN_RATIO) + int(self.num_samples * VAL_RATIO)
 
         image_path, label = self.image_paths[idx]
 
@@ -50,17 +105,17 @@ class ImageDataset(Dataset):
         image_tensor = torchvision.io.read_image(image_path) # C,H,W
 
         # get integer encoding of label
-        label_id = self.label2id[label]
+        class_id = self.class2id[label]
 
-        return image_tensor, label_id
+        return image_tensor, class_id
 
     def __len__(self):
         if self.split == "train":
-            return int(self.total_samples * TRAIN_RATIO)
+            return int(self.num_samples * TRAIN_RATIO)
         elif self.split == "val":
-            return int(self.total_samples * VAL_RATIO)
+            return int(self.num_samples * VAL_RATIO)
         elif self.split == "test":
-            return int(self.total_samples * TEST_RATIO)
+            return int(self.num_samples * TEST_RATIO)
         else:
             raise Exception
 
