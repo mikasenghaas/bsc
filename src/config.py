b2c7f1844918de920f3bbec82884b381f28ad374
jonas-mika <accounts@jonassenghaas.de>
2023-03-06 11:44:45 +0100

    feat(config): updated some default arguments + added dynamic (sub) list of all classes

--- src/config.py
+++ src/config.py
@@ -3,24 +3,32 @@
 
 import os
 import torch
+import glob
+
+# GENERAL
+SEED        : int                   = 1
+DEVICE      : str                   = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 
 # PATHS
 RAW_DATA_PATH : str                 = os.path.join("src", "data", "raw")
 PROCESSED_DATA_PATH : str           = os.path.join("src", "data", "processed")
 MODEL_PATH  : str                   = os.path.join("src", "models")
 
-# GENERAL
-DEVICE      : str                   = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+# CLASSES
+CLASSES : list[str]                 = [x.split('/')[-1] for x in glob.glob(os.path.join(PROCESSED_DATA_PATH, "**"))]
+GROUND_FLOOR : list[str]            = [x for x in CLASSES if x.find('Ground_Floor') >= 0]
+FIRST_FLOOR : list[str]             = [x for x in CLASSES if x.find('First_Floor') >= 0 or x == "Stairs_Atrium"]
 
 # PREPROCESS
-# NUM_FRAMES  : int                  = 16
 MAX_LENGTH  : int                   = 10 
-FPS         : int                   = 2
+FPS         : int                   = 4
 HEIGHT      : int                   = 224
 WIDTH       : int                   = 224
 
 # DATA
 SPLITS      : list[str]             = ["train", "val", "test"]
+SPLIT       : str                   = "test"
+RATIO       : float                 = 1.0
 TRAIN_RATIO : float                 = 0.7
 VAL_RATIO   : float                 = 0.1
 TEST_RATIO  : float                 = 0.2
@@ -31,9 +39,9 @@ STD         : torch.Tensor          = torch.tensor([0.229, 0.224, 0.225])
 
 # TRAINING
 PRETRAINED  : bool                  = True
-MAX_EPOCHS  : int                   = 30
+MAX_EPOCHS  : int                   = 10
 BATCH_SIZE  : int                   = 32
-LR          : float                 = 1e-3
+LR          : float                 = 1e-4
 STEP_SIZE   : int                   = 5
 GAMMA       : float                 = 0.1
 LOG         : bool                  = False
