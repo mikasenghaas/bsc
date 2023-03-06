75d35cff8b5fe79e27d129eb5d477dc7e4251799
jonas-mika <accounts@jonassenghaas.de>
2023-03-06 18:52:16 +0100

    feat(train): minor updates

--- src/train.py
+++ src/train.py
@@ -61,7 +61,7 @@ def train(model, transform, train_loader, val_loader, criterion, optim, schedule
             pbar.set_description(f'{str(epoch).zfill(len(str(args.max_epochs)))}/{str(batch_num).zfill(len(str(len(train_loader))))} ({round(running_training_time / samples_seen * 1000, 1)}ms | {round(running_inference_time / samples_seen * 1000, 1)}ms) - Train: {train_loss:.3f} ({(train_acc * 100):.1f}%) - Val: {val_loss:.3f} ({(val_acc * 100):.1f}%)')
 
             # log epoch metrics for train and val split
-            if args.log:
+            if args.wandb_log:
                 wandb.log({
                     'training_accuracy': train_acc, 
                     'validation_accuracy': val_acc,
@@ -96,7 +96,7 @@ def train(model, transform, train_loader, val_loader, criterion, optim, schedule
         scheduler.step()
 
     # log average training step time/ sample + inference time/ sample
-    if args.log:
+    if args.wandb_log:
         wandb.config.update({
             "training_time_per_sample_ms" : round(sum(training_times) / len(training_times), 1),
             "inference_time_per_sample_ms" : round(sum(inference_times) / len(inference_times), 1)
@@ -111,8 +111,13 @@ def main():
     args = load_train_args()
 
     # initialise wandb run
-    if args.log:
-        wandb.init(project="bsc", name=args.model, notes="computer vision models for indoor localisation", config=vars(args))
+    if args.wandb_log:
+        wandb.init(
+                project="bsc", 
+                group=args.wandb_group if args.wandb_group else None, 
+                name=args.wandb_name if args.wandb_name else None, 
+                tags=args.wandb_tags if args.wandb_tags else None, 
+                config=vars(args))
 
         wandb.define_metric("training_loss", summary="min")
         wandb.define_metric("validation_loss", summary="min")
@@ -122,15 +127,25 @@ def main():
     # load data 
     start_task("Initialising Data and Model")
 
-    # initialise data, tranforms and model
-    data = { split: ImageDataset(filepath=PROCESSED_DATA_PATH, split=split) for split in SPLITS }
+    # initialise data
+    data = { split: ImageDataset(split="train", include_classes=args.include_classes, ratio=args.ratio) for split in SPLITS } 
+    id2class, class2id = data["train"].id2class, data["train"].class2id
+
+    # initialise transforms
     transform = ImageTransformer()
-    model = FinetunedImageClassifier(args.model, pretrained=args.pretrained, id2label=data['train'].id2label)
+
+    # initialise model
+    model = FinetunedImageClassifier(
+            model_name=args.model,
+            num_classes=len(args.include_classes),
+            pretrained=args.pretrained, 
+            id2class=id2class,
+            class2id=class2id)
 
     # initialise data loader
     loader = { split: DataLoader(data[split], batch_size=args.batch_size) for split in SPLITS}
 
-    # define loss func, optimiser and lr scheduler
+    # define loss, optimiser and lr scheduler
     criterion = nn.CrossEntropyLoss() # pyright: ignore
     optim = torch.optim.Adam(model.parameters(), lr=args.lr)
     scheduler = torch.optim.lr_scheduler.StepLR(optim, args.step_size, args.gamma)
@@ -140,19 +155,22 @@ def main():
     print(get_summary(vars(args)))
     trained_model = train(model, transform, loader['train'], loader['val'], criterion, optim, scheduler, args)
 
-    if args.log:
+    if args.wandb_log:
         # log meta information
         wandb.config.update({"num_params": model.meta['num_params']})
+        print(data['train'].meta)
+        print(model.meta)
         wandb.summary["dataset"] = data['train'].meta 
-        wandb.summary["model"] = model.meta
+        # wandb.summary["model"] = model.meta
 
         # prepare artifact saving
         filepath = os.path.join(MODEL_PATH, args.model)
         mkdir(filepath)
 
-        # save transforms
+        # save transforms and model
         start_task(f"Saving Transforms and Model to {filepath}")
         save_pickle(transform, os.path.join(filepath, f"transforms.pkl"))
+        save_json(trained_model.meta, os.path.join(filepath, f"config.json"))
         torch.save(trained_model.state_dict(), os.path.join(filepath, f"{args.model}.pt"))
 
         # save as artifact to wandb
@@ -162,14 +180,14 @@ def main():
         wandb.log_artifact(artifact)
 
         start_task(f"Evaluating Model")
-        labels = data['test'].labels
-        id2label = data['test'].id2label
+        classes = data['test'].classes
+        id2class = data['test'].id2class
         y_true, y_pred, y_probs = get_predictions(trained_model, transform, loader['test'], device=args.device)
 
         wandb.run.summary["test_accuracy"] = np.mean(y_true == y_pred) # pyright: ignore
 
         # wandb visualisation
-        conf_matrix = wandb.plot.confusion_matrix(None, list(y_true), list(y_pred), labels) # pyright: ignore
+        conf_matrix = wandb.plot.confusion_matrix(None, list(y_true), list(y_pred), classes) # pyright: ignore
         roc_curve = wandb.plot.roc_curve(y_true, y_probs) # pyright: ignore
         pr_curve = wandb.plot.pr_curve(y_true, y_probs) # pyright: ignore
 
@@ -180,7 +198,7 @@ def main():
                 idx = batch_num * i + i
                 true, pred = y_true[idx], y_pred[idx] # pyright: ignore
                 if true != pred and len(mispredictions) < 20:
-                    mispredictions.append(wandb.Image(unnormalise_image(image), caption=f"True: {id2label[true]} / Pred: {id2label[pred]}"))
+                    mispredictions.append(wandb.Image(unnormalise_image(image), caption=f"True: {id2class[true]} / Pred: {id2class[pred]}"))
 
         wandb.log({
             'confusion_matrix': conf_matrix,
