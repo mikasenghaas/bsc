b60417b48558e4ddb762ffe6a105290f04a4d55f
jonas-mika <accounts@jonassenghaas.de>
2023-03-13 12:54:31 +0100

    feat(infer): updated infer.py to use cv2 for real-time prediction
    also using new args for specifying what split to sample from and which clip id

--- src/infer.py
+++ src/infer.py
@@ -1,6 +1,7 @@
 import random
 import glob
 
+import cv2
 from torch.nn.functional import softmax
 import torchvision
 from torchvision import transforms
@@ -12,25 +13,18 @@ from model import FinetunedImageClassifier
 from utils import *
 
 def main():
+    # start src/infer.py
+    start = start_task("Running src/infer.py", get_timer=True)
+
     # load args
     args = load_infer_args()
-
-    # load video
-    clip_paths = glob.glob(os.path.join(RAW_DATA_PATH, "*"))
-    video_path = os.path.join(random.choice(clip_paths), "video.mov")
-    meta = load_metadata(video_path)
-    duration = int(eval(meta['duration']))
-    start_time = random.randint(0, duration - args.duration)
-
-    # read video
-    start_task(f"Reading {video_path}")
-    video, _, _ = torchvision.io.read_video(video_path, start_pts=start_time, end_pts=start_time+args.duration, pts_unit="sec", output_format="TCHW")
 
     # load artifact
     start_task(f"Loading {args.model}:{args.version}")
     api = wandb.Api()
     artifact = api.artifact(f'mikasenghaas/bsc/{args.model}:{args.version}', type='model') # pyright: ignore
-    artifact.download()
+    if not os.path.exists(os.path.join(BASEPATH, "artifacts", f"{args.model}:{args.version}")):
+        artifact.download()
 
     # load data
     config = ImageDataset.default_config()
@@ -53,29 +47,53 @@ def main():
     # set eval mode
     model.eval()
 
-    # predict on random video clip
-    fig, ax = plt.subplots()
-    ax.set_title(f"{args.model}:{args.version}") # type: ignore
-    ax.set_xticks([]) # type: ignore
-    ax.set_yticks([]) # type: ignore
+    # get video path
+    if args.clip is None:
+        # random video path in split
+        clip_paths = glob.glob(os.path.join(RAW_DATA_PATH, args.split, "*"))
+        video_path = os.path.join(random.choice(clip_paths), "video.mov")
+    else:
+        # specific video path in split
+        video_path = os.path.join(RAW_DATA_PATH, args.split, args.clip, "video.mov")
 
-    img = ax.imshow(transforms.ToPILImage()(video[0])) # type: ignore
+    # set up video capture
+    cap = cv2.VideoCapture(video_path) # type: ignore
 
-    def animate(i):
-        # transforms
-        logits = model(transform(video[i]).unsqueeze(0))
+    start_task(f"Starting Inference on {'/'.join(video_path.split('/')[-3:])}")
+    while True:
+        # read next frame
+        _, frame = cap.read()
+        if type(frame) != np.ndarray:
+            break
+
+        frame_tensor = torch.tensor(frame).permute(2,0,1) # C, H, W
+        frame_tensor = frame_tensor[[2,1,0], :, :] # change channel to RGB from BGR
+        
+        # predict frame
+        logits = model(transform(frame_tensor).unsqueeze(0))
         probs = softmax(logits, 1)
         prob, pred = torch.max(probs, 1)
         prob, pred = prob.item(), pred.item()
+        class_label = id2class[pred]
+        
+        text = f"{class_label} ({round(100 * prob, 1)}%)"
+            
+        # overlay the prediction on the frame
+        cv2.putText(frame, text, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 255, 0), 1) # type: ignore
+        
+        # display the frame with the prediction overlaid
+        cv2.imshow(f"{args.model}:{args.version}", frame) # type: ignore
+        
+        # exit the loop if the 'q' key is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'): # type: ignore
+            break
 
-        print(f"Prediction: {id2class[pred]} (Confidence: {round(prob * 100, 2)}%)", end="\r")
+    # release the video capture and close the window
+    cap.release()
+    cv2.destroyAllWindows() # type: ignore
 
-        img.set_array(transforms.ToPILImage()(video[i])) # type: ignore
-
-        return [img]
-
-    a = animation.FuncAnimation(fig, animate, frames=len(video), interval=1, blit=True)
-    plt.show()
+    # start src/infer.py
+    end_task("src/infer.py", start)
 
 if __name__ == "__main__":
     main()
