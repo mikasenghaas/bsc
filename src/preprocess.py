6e2144818c2332ba3b968ef0ab85e18b437ca688
Mika Senghaas <accounts@mikasenghaas.de>
2023-04-19 15:48:47 +0200

    feat(preprocess): preprocess per clip, not per class

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,13 +8,11 @@ from tqdm import tqdm
 from config import (
     RAW_DATA_PATH,
     PROCESSED_DATA_PATH,
-    FPS,
 )
 from utils import (
     load_preprocess_args,
     load_metadata,
     load_annotations,
-    timestamp_to_second,
     get_label,
     start_task,
     end_task,
@@ -27,11 +25,10 @@ def main():
     """
     Main function of the preprocess.py script.
 
-    Preprocesses raw videos in train or test split in the following steps:
-    1. Load video into tensor
-    2. Load annotations
-    3. Extract clips of maximal seconds within the boundaries of labels
-    4. Save extracted clips into directory corresponding to label  to disk using ffmpeg
+    Preprocesses raw videos in the filepath `src/data/raw/train` or `src/data/raw/test`
+    and saves the extracted clips into the filepath `src/data/processed/train` or
+    `src/data/processed/test`. The clips are saved into a directory
+    corresponding with the directory name and the labels are saved into a file
     """
     # load args
     args = load_preprocess_args()
@@ -45,73 +42,47 @@ def main():
 
     # iterate over all videos
     pbar = tqdm(directories)
-    for date in pbar:
+    for video_id in pbar:
         # set paths
-        directory = os.path.join(filepath, date)
+        directory = os.path.join(filepath, video_id)
         video_path = os.path.join(directory, "video.mov")
         annotation_path = os.path.join(directory, "annotations")
 
         # set progress bar
-        pbar.set_description(f"Extracting data/{args.split}/{date}")
+        pbar.set_description(f"Extracting data/{args.split}/{video_id}")
 
         # load metadata
-        meta = load_metadata(video_path)
-        duration = int(eval(meta["duration"]))
+        duration, frame_count = load_metadata(video_path)
 
         # meta information
         annotations = load_annotations(annotation_path)
-        seconds = [timestamp_to_second(ts) for ts, _ in annotations]
 
-        start_time = 0  # start time of extraction
-        clip_num = 0  # number of clips extracted from video
-        next_label = 1  # idx of seconds array of next label
+        # make processed filepath
+        savepath = os.path.join(PROCESSED_DATA_PATH, args.split, video_id)
+        mkdir(savepath)
 
-        # extract for as long as video is not over
-        while start_time < duration:
-            # max time allowed (either video duration or max clip length)
-            max_time = min(duration, start_time + args.max_length)
-            if next_label < len(seconds):
-                # get next label time if exists
-                next_label_time = seconds[next_label]
+        # write labels of extracted frames to file
+        labels_path = os.path.join(savepath, "labels.txt")
+        with open(labels_path, "w") as f:
+            for second in range(duration):
+                for _ in range(args.fps):
+                    label = get_label(second, annotations)
+                    f.write(f"{label}\n")
 
-                # set end time of extracted clip
-                if next_label_time < max_time:
-                    end_time = next_label_time
-                    next_label += 1
-                else:
-                    end_time = max_time
-            else:
-                end_time = max_time
+        # compile ffmpeg command to extract frames
+        ffmpeg_command = (
+            f"ffmpeg "
+            f"-loglevel error "
+            f"-y "
+            f"-i {video_path} "
+            f"-vf scale=224:224 "
+            f"-t {duration} "
+            f"-r {args.fps} "
+            f"{savepath}/%03d.jpg"
+        )
 
-            # extract label depending on clip time
-            label = get_label(start_time, annotations)
-            destination_dir = os.path.join(
-                PROCESSED_DATA_PATH,
-                args.split,
-                label,
-                f"{date}_{str(clip_num).zfill(2)}",
-            )
-            mkdir(destination_dir)
-
-            # compile ffmpeg commands
-            ffmpeg_command = (
-                f"ffmpeg "
-                f"-loglevel error "
-                f"-ss {start_time} "
-                f"-y "
-                f"-i {video_path} "
-                f"-vf scale=224:224 "
-                f"-t {end_time-start_time} "
-                f"-r {FPS} "
-                f"{destination_dir}/%02d.jpg"
-            )
-
-            # run command
-            os.system(ffmpeg_command)
-
-            # update loop information
-            start_time = end_time
-            clip_num += 1
+        # run command
+        os.system(ffmpeg_command)
 
     end_task("Processing Raw Videos", start_timer)
 
